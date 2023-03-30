b02d8cc61f11553d2c8be89c5a37efd9235187fd
Luqui87 <alvarezlucas2787@gmail.com>
2023-03-30 10:57:45 -0300

    Comentarios eliminados

--- entrega-1-alvarez-lucas/primer_juego_2D/Main.gd
+++ entrega-1-alvarez-lucas/primer_juego_2D/Main.gd
@@ -2,18 +2,8 @@ extends Node
 export(PackedScene) var mob_scene
 var score
 
-
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 	
 func new_game():
